44857ab2f31257d03be130f974eb03d734ce01dd
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-11-17 17:22:01 +0100

    Prévisualisation de l’image du formulaire

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ const divPlus = document.querySelector(".divPlus");
 plus.addEventListener("click", () => {
     formulaire.classList.remove("hidden");
 });
+
+// Prévisualisation de l’image du formulaire
+const lien = document.querySelector(".lien");
+const imgDiv = document.querySelector(".imgDiv");
+
+lien.addEventListener("input", () => {
+    imgDiv.style.backgroundImage = `url('${lien.value}')`;
+    imgDiv.style.backgroundSize = "cover";
+    imgDiv.style.backgroundPosition = "center";
+});
