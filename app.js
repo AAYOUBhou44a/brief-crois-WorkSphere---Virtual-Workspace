dc7fa720c4729718fcf0b869939a9df697bc9a72
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-11-17 17:27:30 +0100

    Création de la carte de l’employé et envoi dans Unassigned

--- app.js
+++ app.js
@@ -41,3 +41,28 @@ submit.addEventListener("click", (evenement) => {
     employees.push(employe);
     formulaire.reset();
 });
+
+// Création de la carte de l’employé et envoi dans Unassigned
+submit.addEventListener("click", () => {
+
+    const employe = employees[employees.length - 1];
+
+    const personne = document.createElement("div");
+    personne.className = "w-full h-[80px] flex items-center justify-evenly bg-red-500";
+
+    const imagePersonne = document.createElement("div");
+    imagePersonne.className = `border rounded-[50%] h-[90%] w-[30%] bg-[url('${employe.image}')] bg-cover bg-center`;
+    personne.appendChild(imagePersonne);
+
+    const infos = document.createElement("div");
+    infos.className = "flex gap-[20px]";
+    infos.innerHTML = `
+        <h1>${employe.nom}</h1>
+        <h1>${employe.role}</h1>
+    `;
+
+    personne.appendChild(infos);
+
+    UnassignedDivs.push(personne);
+    divPlus.before(personne);
+});
