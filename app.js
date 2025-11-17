631ae534d110bed1f28d2360ecd39dd96d196fe9
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-11-17 17:19:55 +0100

    l'affiche du formaulaire en cliquant sur le plus bouton

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const main = document.querySelector("main");
 const plus = document.querySelector(".plus");
 const formulaire = document.querySelector(".formulaire");
 const divPlus = document.querySelector(".divPlus");
+
+plus.addEventListener("click", () => {
+    formulaire.classList.remove("hidden");
+});
