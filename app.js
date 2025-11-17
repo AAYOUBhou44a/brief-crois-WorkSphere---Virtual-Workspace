f26a0491a8c770feaab513073a8f343ed7b64997
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-11-17 17:24:21 +0100

    Récupération des données du formulaire en cliquant sur le bouton submit

--- app.js
+++ app.js
@@ -17,3 +17,27 @@ lien.addEventListener("input", () => {
     imgDiv.style.backgroundSize = "cover";
     imgDiv.style.backgroundPosition = "center";
 });
+
+// Récupération des données du formulaire
+const employees = [];
+const UnassignedDivs = [];
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("click", (evenement) => {
+
+    evenement.preventDefault();
+    formulaire.classList.add("hidden");
+
+    const employe = {
+        image: lien.value,
+        nom: document.querySelector(".nom").value,
+        role: document.querySelector(".role").value,
+        email: document.querySelector(".email").value,
+        telephone: document.querySelector(".tel").value,
+        experiences: document.querySelector(".number").value,
+        location: document.querySelector(".location").value
+    };
+
+    employees.push(employe);
+    formulaire.reset();
+});
